8905dd2fabf79bbd5f82589ccfe411e1bdfb9310
Varga Roland <106912409+vargaRolandGit@users.noreply.github.com>
2024-12-17 23:21:49 +0100

    Mobile

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar";
 import Landing from "./components/Landing";
 import WhyUs from "./components/WhyUs";
 import Footer from "./components/Footer";
-import Mobile from "./components/mobile";
+import Mobile from "./components/Mobile";
 
 
 export default () => (
